5e52b9799ac9a3439fbb7cd46deb0553f0aa8bb3
Nur Nedim Okatan <103523460+nurNedimOkatan@users.noreply.github.com>
2024-07-03 19:51:16 +0300

    Update PublishAspNet5Website.ps1

--- scripts/PublishAspNet5Website.ps1
+++ scripts/PublishAspNet5Website.ps1
@@ -16,6 +16,9 @@ param(
 	
 	[Parameter(Mandatory=$true)]
 	[string]$deployUserPassword
+	
+	[Parameter(Mandatory=$true)]
+	[switch]$deploySkipExtraFilesOnServer
 )
 
 Write-Host "Deployment starting..."
@@ -25,16 +28,16 @@ $publishProperties = @{'WebPublishMethod'='MSDeploy';
                         'DeployIisAppPath'=$websiteName;
                         'Username'=$deployUserName;
                         'Password'=$deployUserPassword;
-						'SkipExtraFilesOnServer'=$false;
-						'EnableMSDeployAppOffline'=$true;
-						'ExcludeFiles'=@(
-							@{'objectname'='filePath';'absolutepath'='.*google.*\.html'},
-							@{'objectname'='filePath';'absolutepath'='.*BingSiteAuth\.xml'},
-							@{'objectname'='filePath';'absolutepath'='logs\\.*'},
-							@{'objectname'='dirPath';'absolutepath'='logs'},
-							@{'objectname'='filePath';'absolutepath'='data\\.*'},
-							@{'objectname'='dirPath';'absolutepath'='data'}
-						)}
+			'SkipExtraFilesOnServer'=$deploySkipExtraFilesOnServer;
+			'EnableMSDeployAppOffline'=$true;
+			'ExcludeFiles'=@(
+				@{'objectname'='filePath';'absolutepath'='.*google.*\.html'},
+				@{'objectname'='filePath';'absolutepath'='.*BingSiteAuth\.xml'},
+				@{'objectname'='filePath';'absolutepath'='logs\\.*'},
+				@{'objectname'='dirPath';'absolutepath'='logs'},
+				@{'objectname'='filePath';'absolutepath'='data\\.*'},
+				@{'objectname'='dirPath';'absolutepath'='data'}
+			)}
 
 
 $publishScript = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'default-publish.ps1'
